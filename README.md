a428033804f4f4639dec1a51a66de8d313b55a5f
kyle <kylejaycampbell@gmail.com>
2025-08-02 12:04:36 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Pipecat AI Avatar Agents
+
+This is an example of a AI avatars that use the Pipecat SDK to create an AI agent that can be used in a Daily room.
+
+## Installation
+
+```console
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Setup
+
+In this project's directory, run the following command to copy the `.env.example` file to `.env`:
+
+```console
+cp .env.example .env
+```
+
+Edit the `.env` file with your own values.
+
+### OpenAI - for STT, LLM, and TTS
+
+Visit https://platform.openai.com to get your `OPENAI_API_KEY`.
+
+### Tavus - for avatar
+
+Visit https://tavus.io to get your `TAVUS_API_KEY` and `TAVUS_REPLICA_ID`.
+
+OR
+
+### HeyGen - for avatar
+
+Unfortunately, to use HeyGen's interactive avatar, you must pay for creator plan 😔
+Visit https://heygen.com to get your `HEYGEN_API_KEY`.
+
+## Usage
+
+Run the following command to start the agent:
+```console
+python avatar_tavus.py
+or
+python avatar_heygen.py
+```
+
+Then, from the logs in above, find and click on or copy the `http://localhost:7860/client` and paste it into your browser, or just go there.
+
+
+## References
+
+- [Pipecat AI Tavus Documentation](https://docs.pipecat.ai/server/services/video/tavus)
+- [Pipecat AI GitHub Tavus Example](https://github.com/pipecat-ai/pipecat/blob/main/examples/foundational/21a-tavus-video-service.py)
+- [Pipecat AI GitHub HeyGen Example](https://github.com/pipecat-ai/pipecat/blob/main/examples/foundational/43a-heygen-video-service.py)
---
+++ avatar_tavus.py
@@ -0,0 +1,137 @@
+#
+# Copyright (c) 2024–2025, Daily
+#
+# SPDX-License-Identifier: BSD 2-Clause License
+#
+
+
+import os
+
+import aiohttp
+from dotenv import load_dotenv
+from loguru import logger
+
+from pipecat.audio.vad.silero import SileroVADAnalyzer
+from pipecat.pipeline.pipeline import Pipeline
+from pipecat.pipeline.runner import PipelineRunner
+from pipecat.pipeline.task import PipelineParams, PipelineTask
+from pipecat.processors.aggregators.openai_llm_context import OpenAILLMContext
+from pipecat.runner.types import RunnerArguments
+from pipecat.runner.utils import create_transport
+from pipecat.services.openai.tts import OpenAITTSService
+from pipecat.services.openai.stt import OpenAISTTService
+from pipecat.services.openai.llm import OpenAILLMService
+from pipecat.services.tavus.video import TavusVideoService
+from pipecat.transports.base_transport import BaseTransport, TransportParams
+from pipecat.transports.services.daily import DailyParams
+
+load_dotenv(override=True)
+
+# We store functions so objects (e.g. SileroVADAnalyzer) don't get
+# instantiated. The function will be called when the desired transport gets
+# selected.
+transport_params = {
+    "daily": lambda: DailyParams(
+        audio_in_enabled=True,
+        audio_out_enabled=True,
+        video_out_enabled=True,
+        video_out_is_live=True,
+        video_out_width=1280,
+        video_out_height=720,
+        vad_analyzer=SileroVADAnalyzer(),
+    ),
+    "webrtc": lambda: TransportParams(
+        audio_in_enabled=True,
+        audio_out_enabled=True,
+        video_out_enabled=True,
+        video_out_is_live=True,
+        video_out_width=1280,
+        video_out_height=720,
+        vad_analyzer=SileroVADAnalyzer(),
+    ),
+}
+
+
+async def run_bot(transport: BaseTransport):
+    logger.info(f"Starting bot")
+    async with aiohttp.ClientSession() as session:
+        stt = OpenAISTTService(api_key=os.getenv("OPENAI_API_KEY"))
+
+        tts = OpenAITTSService(
+            api_key=os.getenv("OPENAI_API_KEY"),
+            voice_id="alloy",
+        )
+
+        llm = OpenAILLMService(api_key=os.getenv("OPENAI_API_KEY"))
+
+        tavus = TavusVideoService(
+            api_key=os.getenv("TAVUS_API_KEY"),
+            replica_id=os.getenv("TAVUS_REPLICA_ID"),
+            session=session,
+        )
+
+        messages = [
+            {
+                "role": "system",
+                "content": "You are a helpful LLM in a WebRTC call. Your goal is to demonstrate your capabilities in a succinct way. Your output will be converted to audio so don't include special characters in your answers. Respond to what the user said in a creative and helpful way.",
+            },
+        ]
+
+        context = OpenAILLMContext(messages)
+        context_aggregator = llm.create_context_aggregator(context)
+
+        pipeline = Pipeline(
+            [
+                transport.input(),  # Transport user input
+                stt,  # STT
+                context_aggregator.user(),  # User responses
+                llm,  # LLM
+                tts,  # TTS
+                tavus,  # Tavus output layer
+                transport.output(),  # Transport bot output
+                context_aggregator.assistant(),  # Assistant spoken responses
+            ]
+        )
+
+        task = PipelineTask(
+            pipeline,
+            params=PipelineParams(
+                audio_in_sample_rate=16000,
+                audio_out_sample_rate=24000,
+                enable_metrics=True,
+                enable_usage_metrics=True,
+            ),
+        )
+
+        @transport.event_handler("on_client_connected")
+        async def on_client_connected(transport, client):
+            logger.info(f"Client connected")
+            # Kick off the conversation.
+            messages.append(
+                {
+                    "role": "system",
+                    "content": "Start by greeting the user and ask how you can help.",
+                }
+            )
+            await task.queue_frames([context_aggregator.user().get_context_frame()])
+
+        @transport.event_handler("on_client_disconnected")
+        async def on_client_disconnected(transport, client):
+            logger.info(f"Client disconnected")
+            await task.cancel()
+
+        runner = PipelineRunner(handle_sigint=False)
+
+        await runner.run(task)
+
+
+async def bot(runner_args: RunnerArguments):
+    """Main bot entry point compatible with Pipecat Cloud."""
+    transport = await create_transport(runner_args, transport_params)
+    await run_bot(transport)
+
+
+if __name__ == "__main__":
+    from pipecat.runner.run import main
+
+    main()
